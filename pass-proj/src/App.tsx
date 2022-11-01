bb00c221090546165bd4397499c469361aa2c5a6
MOhammed haimi <mohammed@haimi.org>
2022-11-02 01:22:14 +0800

    new template installation

--- pass-proj/src/App.tsx
+++ pass-proj/src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar'
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <NavBar/>
+      <Router>
+        <Routes>
+
+          <Route path="/signup" element={
+            <SignUp />
+          } />
+        </Routes>
+        <Routes>
+
+          <Route path="/" element={
+            <HomePage />
+          } />
+        </Routes>
+
+
+
+      </Router>
+
+    </ ChakraProvider>
   );
 }
 
